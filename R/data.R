c34411137f24507fa539bf3aa04c4b8385a9dbc1
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2016-01-04 17:18:00 -0600

    Document D-I reference datasets

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Subdivisions and conferences
+#'
+#' A dataset containing the conferences of the two NCAA Division-I
+#' subdivisions.
+#'
+#' @format A data frame with 25 rows and 2 variables:
+#' \itemize{
+#'  \item DIV: NCAA Division-I subdivision (FBS, FCS)
+#'  \item CONF: Athletic conference
+#' }
+"confs"
+
+#' Database identifiers for teams
+#'
+#' A dataset containing teams and their online database identifiers.
+#'
+#' @format A data frame with 253 rows and 3 variables:
+#' \itemize{
+#'  \item CONF: Athletic conference
+#'  \item TEAM: NCAA Division-I team
+#'  \item ESPN.ID: espn.go.com team identifier
+#' }
+"teams"
